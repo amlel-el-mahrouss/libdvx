7c8d8db451f5ea1bdee9ecb2013a5e5defb58a1c
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-12-09 13:33:51 +0100

    IMP: Pushing to a public repository.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- inc/dvx_stream.h
+++ inc/dvx_stream.h
@@ -1,6 +1,6 @@
 /* -------------------------------------------
 
-	Copyright (C) 2024 ELMH GROUP, all rights reserved.
+	Copyright (C) 2024 Amlal EL Mahrouss, all rights reserved.
 
 ------------------------------------------- */
 
@@ -8,7 +8,7 @@
 #define DVX_STREAM_H
 
 /// @brief Digital Video eXtended C++ library.
-/// @author ELMH GROUP.
+/// @author Amlal EL Mahrouss.
 
 #include <dvx_core_api.h>
 
