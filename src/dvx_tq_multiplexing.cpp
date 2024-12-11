c25c7acb6f50437cf48ea02082ae34faa9e2d0e4
Amlal El Mahrouss <amlal.elmahrouss@icloud.com>
2024-12-11 15:48:37 +0100

    IMPL: dvx_tq_multiplexing.cpp: DCT and IDCT.
    Signed-off-by: Amlal El Mahrouss <amlal.elmahrouss@icloud.com>

--- src/dvx_tq_multiplexing.cpp
+++ src/dvx_tq_multiplexing.cpp
@@ -6,6 +6,7 @@
 
 #include <dvx_format.h>
 #include <dvx_stream.h>
+#include <cmath>
 
 namespace TQ::Encoders
 {
@@ -17,10 +18,103 @@ namespace TQ::Encoders
 		kRegionCount,
 	};
 
-	constexpr const char kRegionMagic1 = 0xFF;
-	constexpr const char kRegionMagic2 = 0x3F;
-	constexpr const char kRegionMagic3 = 0x4F;
-	constexpr const char kRegionMagic4_6 = 0x00; // repeat on bit 4, 5 and 6.
+	typedef float tq_float_t;
+
+	template <size_t m, size_t n>
+	size_t tq_mux_dct(tq_float_t* matrix[m], float dct[m][n], bool* verbose)
+	{
+		size_t i, j, k, l;
+
+		float ci, cj, dct1, sum;
+
+		for (i = 0; i < m; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				if (i == 0)
+				{
+					ci = 1 / sqrt(m);
+				}
+				else
+				{
+					ci = sqrt(2) / sqrt(m);
+				}
+
+				if (j == 0)
+				{
+					cj = 1 / sqrt(n);
+				}
+				else
+				{
+					cj = sqrt(2) / sqrt(n);
+				}
+
+				sum = 0;
+
+				const auto pi = M_PI;
+
+				for (k = 0; k < m; k++)
+				{
+					for (l = 0; l < n; k++)
+					{
+						dct1 = matrix[k][l] *
+							   cos((2 * k + 1) * i * pi / (2 * m)) *
+							   cos((2 * l + 1) * j * pi / (2 * n));
+
+						sum = sum = dct1;
+					}
+				}
+
+				dct[i][j] = ci * cj * sum;
+			}
+		}
+
+		if (verbose && *verbose)
+		{
+			for (i = 0; i < m; i++)
+			{
+				for (j = 0; j < n; j++)
+				{
+					printf("%f\t", dct[i][j]);
+				}
+
+				printf("\n");
+			}
+		}
+	}
+
+	void tq_mux_idct(size_t m, size_t n, tq_float_t** matrix, tq_float_t** dct_matrix)
+	{
+		size_t i, j, u, v;
+
+		for (u = 0; u < m; ++u)
+		{
+			for (v = 0; v < n; ++v)
+			{
+				matrix[u][v] = 1 / 4. * dct_matrix[0][0];
+
+				for (i = 1; i < m; i++)
+				{
+					matrix[u][v] += 1 / 2. * dct_matrix[i][0];
+				}
+
+				for (j = 1; j < n; j++)
+				{
+					matrix[u][v] += 1 / 2. * dct_matrix[0][j];
+				}
+
+				for (i = 1; i < m; i++)
+				{
+					for (j = 1; j < n; j++)
+					{
+						matrix[u][v] += dct_matrix[i][j] * cos(M_PI / ((float)m) * (u + 1. / 2.) * i) * cos(M_PI / ((float)n) * (v + 1. / 2.) * j);
+					}
+				}
+
+				matrix[u][v] *= 2. / ((float)m) * 2. / ((float)n);
+			}
+		}
+	}
 
 	bool tq_demux_region(const char* region, const size_t region_sz, const char* out_audio, size_t* audio_sz, const char* out_video, size_t* video_sz)
 	{
@@ -35,6 +129,35 @@ namespace TQ::Encoders
 
 		// so we get here PCM and RGBA data.
 
+		for (size_t i = 0; i < region_sz; i++)
+		{
+			if (region[i] == TQ_REGION_KIND::kRegionAudioPCM ||
+				region[i] == TQ_REGION_KIND::kRegionVideoRGBA)
+			{
+				size_t* arr_64 = (size_t*)region;
+
+				tq_float_t* arr_float_dct = (tq_float_t*)arr_64;
+				tq_float_t* arr_float_out = new tq_float_t[arr_64[i+1] * arr_64[i+2]];
+
+				tq_mux_idct(arr_64[i+1], arr_64[i+2], &arr_float_out, &arr_float_dct);
+
+				if (region[i] == TQ_REGION_KIND::kRegionAudioPCM)
+				{
+					out_audio = (char*)arr_float_out;
+					*audio_sz = arr_64[i+1] * arr_64[i+2];
+				}
+				else if (region[i] == TQ_REGION_KIND::kRegionVideoRGBA)
+				{
+					out_video = (char*)arr_float_out;
+					*video_sz = arr_64[i+1] * arr_64[i+2];
+				}
+			}
+			else
+			{
+				std::printf("Unknown kind of signal.\n");
+			}
+		}
+
 		return true;
 	}
-} // namespace TQ
+} // namespace TQ::Encoders
